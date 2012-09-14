ae57dfb199c4839b66995d6df03402e93e069b9a
EKT <kstamatis@ekt.gr>
2012-09-14 15:28:53 +0300

    fix problem with browse ordering

--- dspace-api/src/main/java/org/dspace/browse/BrowseDAOPostgres.java
+++ dspace-api/src/main/java/org/dspace/browse/BrowseDAOPostgres.java
@@ -783,12 +783,13 @@ public class BrowseDAOPostgres implements BrowseDAO
 
         //If we want frequencies and this is not a count query, enchance the query accordingly
         if (isEnableBrowseFrequencies() && countValues==null){
-            String before = "SELECT count(*) AS num, values.value, values.authority FROM (";
+            String before = "SELECT count(*) AS num, values.value, values.authority, values."+orderField+" FROM (";
             String after = ") values , "+tableMap+" WHERE values.id = "+tableMap+".distinct_id GROUP BY "+tableMap+
-                    ".distinct_id, values.value, values.authority;";
+                    ".distinct_id, values.value, values.authority, values.sort_value";
             
             queryBuf.insert(0, before);
             queryBuf.append(after);
+            buildOrderBy(queryBuf);   
         }
         
         return queryBuf.toString();
